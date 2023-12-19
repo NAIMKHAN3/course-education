dbf7918d4d37846cf1452cfa9c12d3b54fdcd439
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-12-19 14:21:17 +0600

    global error handle done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import errorHandler, { notfoundandler } from './middleware/errorHandler'
 
 const app: Application = express()
 
@@ -11,5 +12,8 @@ app.get('/', (req: Request, res: Response) => {
     .status(200)
     .send({ success: true, message: 'Course Education Server is Running' })
 })
+
+app.use(notfoundandler)
+app.use(errorHandler)
 
 export default app
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,4 +5,5 @@ dotenv.config({ path: path.join(process.cwd(), '.env') })
 export default {
   port: process.env.PORT,
   database_url: process.env.MONGODB_URL,
+  env: process.env.env
 }
